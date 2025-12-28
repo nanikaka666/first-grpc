7c93f4268d4bdbf97bdd539e71a828c863d10613
nanikaka <nanikaka666@gmail.com>
2025-12-28 22:24:36 +0900

    refactor: extract helloServiceClient

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,50 +1,5 @@
-import { credentials } from "@grpc/grpc-js";
-import { HelloServiceClient } from "../generatedPb/proto/hello_grpc_pb";
-import {
-    SayHelloRequest,
-    SayHelloResponse,
-} from "../generatedPb/proto/hello_pb";
-
 import { googleWithApiKey } from "./google";
 
-async function main() {
-    const client = new HelloServiceClient(
-        "localhost:9000",
-        credentials.createInsecure()
-    );
-
-    const request = new SayHelloRequest();
-    request.setNum(999);
-    request.setReqarg("Protoc gen!!");
-
-    const response = await new Promise<SayHelloResponse>((resolve, reject) => {
-        client.sayHello(request, (error, value) => {
-            if (error) {
-                console.log(error);
-                reject(error);
-            }
-            if (!value) {
-                console.log("undefined");
-                reject();
-            } else {
-                resolve(value);
-            }
-        });
-    });
-
-    console.log(`Client call response: ${response.getResstring()}`);
-
-    console.log("Start Stream");
-
-    await client.sayManyHello(request).forEach((data) => {
-        const res: SayHelloResponse = data;
-        console.log(`Streaming: ${res.getResstring()}`);
-    });
-
-    console.log("End Stream");
-}
-
 (async () => {
-    // await main();
     await googleWithApiKey();
 })();
